d34d75f7bedb969a559376d2f4dee5c6a60fa005
Swati singh <swatigola274@email.com>
2026-06-08 13:55:59 +0530

    change db for production

--- RoomRent/backend/app.js
+++ RoomRent/backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 
 const app = express()
-const PORT = 9000
+const PORT = process.env.PORT || 9000
 const db = require("./config/db")
 
 require("./models/user")
@@ -9,7 +9,10 @@ require("./models/index")
 const cors = require("cors")
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({ extended: true ,limit:"50mb"}));
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    credentials: true,
+}))
 
 const userRoute = require("./routes/user")
 const propertyRoute = require("./routes/property")
--- RoomRent/backend/config/db.js
+++ RoomRent/backend/config/db.js
@@ -1,13 +1,16 @@
 const { Sequelize } = require("sequelize")
 require("dotenv").config()
 const sequelize = new Sequelize(
-    process.env.APP_NAME,
-    process.env.ROOT,
-    process.env.PASSWORD, {
-    host: "localhost",
-    dialect: "mysql"
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+        dialect: "mysql",
+        logging: false,
 
 })
+   
     ; (async() => {
         try {
             await sequelize.authenticate()
