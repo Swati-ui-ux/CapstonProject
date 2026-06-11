789de6125597dfdc46f4bad0cfd8542fb801b731
Swati singh <swatigola274@email.com>
2026-06-11 13:08:38 +0530

    add live url

--- RoomRent/frontend/src/utils/axiosInstance.js
+++ RoomRent/frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://room-rent-app.onrender.comhttp://localhost:9000",
 });
 
 axiosInstance.interceptors.request.use(
